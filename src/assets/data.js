3208548581e4a882f586803435423818927d8b64
MUNEER629 <muneer772001@gmail.com>
2024-10-29 12:55:43 +0530

    add files

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data=[{
+    question:"Who developed React Js ?",
+    option1:"Jordan Walke",
+    option2:"Rasmus Lerdorf",
+    option3:"Brendan Eich",
+    option4:"Tim Berners-Lee",
+    ans:1,
+},
+{
+    question:"Which method in React is used to update the state of a component ?",
+    option1:"modifyState()",
+    option2:"setState()",
+    option3:"this.state",
+    option4:"updateState()",
+    ans:2,
+},
+{
+    question:"What is JSX in React ?",
+    option1:" A style formatting tool",
+    option2:"A templating engine",
+    option3:"A syntax extension for JavaScript",
+    option4:"A database query language",
+    ans:3,
+},
+{
+    question:"In React, what is the purpose of useEffect hook ?",
+    option1:"To handle user events",
+    option2:"To store data",
+    option3:"To create class components",
+    option4:"To manage side effects in functional components",
+    ans:4,
+},
+{
+    question:"What is the virtual DOM in React?",
+    option1:"A programming interface for web pages",
+    option2:" A copy of the real DOM that React uses to make efficient updates",
+    option3:" A server-side database",
+    option4:"A file system for storing static content",
+    ans:2,
+},
+
+
+];
